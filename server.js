d9c130283ac220c0d45bf4921c52d447bb8981c3
Laurel Thomson <laurel.beth.thomson@gmail.com>
2019-07-03 23:36:25 -0500

    taking out authentication, as its not working properly

--- server.js
+++ server.js
@@ -7,20 +7,7 @@ const serveFile = require('./src/serve-file');
 const serveHome = require('./src/serve-home');
 const serveArrangement = require('./src/serve-arrangement');
 const serveArrangementImage = require('./src/serve-arrangement-image');
-const serveSignup = require('./src/serve-signup');
-const serveSignin = require('./src/serve-signin');
 const serve404 = require('./src/serve404');
-const serveAdmin = require('./src/serve-admin');
-const postArrangement = require('./src/upload-arrangement');
-const serveUploadArrangement = require('./src/serve-upload-arrangement');
-const serveUpdateArrangement = require('./src/serve-update-arrangement');
-const updateArrangementForm = require('./src/serve-update-arrangement-form');
-const updateArrangement = require('./src/update-arrangement');
-const createUser = require('./src/create-user');
-const createSession = require('./src/create-session');
-const loadSession = require('./lib/load-session');
-const serveUpdatePassword = require('./src/serve-update-password');
-const updatePassword = require('./src/update-password');
 
 var router = new Router(serve404);
 var templates = new Templates("./templates");
@@ -33,29 +20,12 @@ router.addRoute("GET", "/index", serveHome);
 router.addRoute("GET", "/index.html", serveHome);
 router.addRoute("GET", "/arrangements/:id", serveArrangement);
 router.addRoute("GET", "/arrangement-images/:id", serveArrangementImage);
-router.addRoute("GET", "/admin", serveAdmin);
-router.addRoute("POST", "/upload-arrangement", postArrangement);
-router.addRoute("GET", "/upload-arrangement", serveUploadArrangement);
-router.addRoute("GET", "/update-arrangement", serveUpdateArrangement);
-router.addRoute("GET", "/update-arrangement-form/:id", updateArrangementForm);
-router.addRoute("POST", "/update-arrangement", updateArrangement);
-router.addRoute("GET", "/signup", serveSignup);
-router.addRoute("POST", "/signup", createUser);
-router.addRoute("GET", "/signin", serveSignin);
-router.addRoute("POST", "/signin", createSession);
-router.addRoute("GET", "/update-password", serveUpdatePassword);
-router.addRoute("POST", "/update-password", updatePassword);
 
 var server = http.createServer({}, (req, res) => {
-  res.templates = templates;
-  // Attempt to load the session before routing
-  loadSession(req, res, (req, res) => {
-    // Route the request
-    router.route(req, res);
-  });
+  res.templates = templates;  
+  router.route(req, res);
 });
 
-// Begin listening for requests
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
